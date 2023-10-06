71a650021b50ce586551f3a052ceca0e83dd3f05
Nader Naderi <2200stellaris@gmail.com>
2023-10-06 16:54:45 +0330

    box 2d added. we have physics.

--- NDR2DGameEngine/main.cpp
+++ NDR2DGameEngine/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include "Game.hpp"
 #include "DEFINITIONS.hpp"
+#include "box2d/box2d.h"
 
 int main()
 {
+	b2Vec2 gravity(0.0f, -9.8f);
+	b2World world(gravity);
+
+
 	ArtaEngine::Game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE);
 	return EXIT_SUCCESS;
 }
